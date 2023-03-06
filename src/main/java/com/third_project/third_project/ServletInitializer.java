41993073910e6df9d019a7ed95c7d0b218ae7a5e
2eehoo <itwit222@gmail.com>
2023-03-06 16:44:48 +0900

    1

---
+++ src/main/java/com/third_project/third_project/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.third_project.third_project;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ThirdProjectApplication.class);
+	}
+
+}
---
+++ src/main/java/com/third_project/third_project/ThirdProjectApplication.java
@@ -0,0 +1,13 @@
+package com.third_project.third_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdProjectApplication.class, args);
+	}
+
+}
