55af63deda0f1fc2b6f98f9493922531f737d664
armycar <credit102@naver.com>
2023-03-13 16:34:15 +0900

    dgc

---
+++ src/main/java/com/third_project/third_project/repository/GenInfoRepository.java
@@ -0,0 +1,7 @@
+package com.third_project.third_project.repository;
+
+import com.third_project.third_project.entity.GenInfoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GenInfoRepository extends JpaRepository<GenInfoEntity, Long> {
+}
