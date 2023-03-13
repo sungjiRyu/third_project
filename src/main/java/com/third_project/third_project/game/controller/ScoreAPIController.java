76a41f3d2d6912c0ba655c0e47e65f52acecdfe7
inwonjung <inwon123@naver.com>
2023-03-13 16:52:13 +0900

    game2

--- src/main/java/com/third_project/third_project/game/controller/ScoreAPIController.java
+++ src/main/java/com/third_project/third_project/game/controller/ScoreAPIController.java
@@ -1,4 +1,11 @@
 package com.third_project.third_project.game.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/game/score")
+@RequiredArgsConstructor
 public class ScoreAPIController {
 }
--- src/main/java/com/third_project/third_project/game/service/ScoreService.java
+++ src/main/java/com/third_project/third_project/game/service/ScoreService.java
@@ -1,4 +1,9 @@
 package com.third_project.third_project.game.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ScoreService {
 }
