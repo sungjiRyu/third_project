880d4a37835f3e7f2c4b0799f668a3e00e9531d5
armycar <credit102@naver.com>
2023-03-14 13:10:34 +0900

    dgc

--- src/main/java/com/third_project/third_project/repository/GameScoreRepository.java
+++ src/main/java/com/third_project/third_project/repository/GameScoreRepository.java
@@ -1,6 +1,7 @@
 package com.third_project.third_project.repository;
 
 import com.third_project.third_project.entity.GameScoreEntity;
+import com.third_project.third_project.entity.MemberInfoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GameScoreRepository extends JpaRepository<GameScoreEntity, Long> {
