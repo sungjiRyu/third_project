e7fb30bf37a63c833ba885ceb83535e952d0fc48
armycar <credit102@naver.com>
2023-03-14 12:43:13 +0900

    dgc

--- src/main/java/com/third_project/third_project/game/service/ScoreService.java
+++ src/main/java/com/third_project/third_project/game/service/ScoreService.java
@@ -1,9 +1,20 @@
 package com.third_project.third_project.game.service;
 
+import com.third_project.third_project.entity.GameScoreEntity;
+import com.third_project.third_project.game.vo.ScoreResponseVO;
+import com.third_project.third_project.repository.GameScoreRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ScoreService {
+    private final GameScoreRepository gsRepo;
+
+    public ScoreResponseVO getMemberScore(Long seq) {
+//        Optional<GameScoreEntity> entityOpt = gsRepo.
+        return null;
+    }
 }
