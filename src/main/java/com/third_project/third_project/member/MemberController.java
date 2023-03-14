4752a051fa4a8aa01a1c3c64018acffc9464fa92
armycar <credit102@naver.com>
2023-03-14 11:35:23 +0900

    1234

--- src/main/java/com/third_project/third_project/member/MemberController.java
+++ src/main/java/com/third_project/third_project/member/MemberController.java
@@ -1,15 +1,15 @@
 package com.third_project.third_project.member;
 
 import com.third_project.third_project.member.VO.MemberAddVO;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
+//import io.swagger.v3.oas.annotations.Operation;
+//import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "회원관련", description = "회원 CRUD")
+//@Tag(name = "회원관련", description = "회원 CRUD")
 @RestController
 @RequestMapping("/api/member")
 @RequiredArgsConstructor
