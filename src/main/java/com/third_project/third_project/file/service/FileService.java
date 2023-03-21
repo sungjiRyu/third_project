df6fca1cd43aff4f682d10db9220eaea3553de53
armycar <credit102@naver.com>
2023-03-21 10:38:32 +0900

    파일경로추가

--- src/main/java/com/third_project/third_project/file/service/FileService.java
+++ src/main/java/com/third_project/third_project/file/service/FileService.java
@@ -18,7 +18,6 @@ public class FileService {
     @Value("${file.video.exercise.game}") String game_video_path;
     @Value("${file.video.exercise.detail}") String detail_video_path;
     @Value("${file.video.exercise.main}") String main_video_path;
-    @Value("${file.image.exercise.game}")   String   game_image_path;
     @Value("${file.image.exercise.detail}") String detail_image_path;
     @Value("${file.image.exercise.main}")   String   main_image_path;
 
@@ -40,6 +39,50 @@ public class FileService {
 
         String filename = "";
         if (type.equals("game")) {
+            if (cvRepo.findByCvUrlEquals(uri)==null) {
+                throw new InvalidInputException("파일이 존재하지 않습니다.");
+            }
+            else {
+                filename = cvRepo.findByCvUrlEquals(uri).getCvName();
+            }
+        }
+        else if (type.equals("detail")) {
+            if (cvRepo.findByCvUrlEquals(uri)==null) {
+                throw new InvalidInputException("파일이 존재하지 않습니다.");
+            }
+            else {
+                filename = cvRepo.findByCvUrlEquals(uri).getCvName();
+            }
+        }
+        else if (type.equals("main")) {
+            if (cvRepo.findByCvUrlEquals(uri)==null) {
+                throw new InvalidInputException("파일이 존재하지 않습니다.");
+            }
+            else {
+                filename = cvRepo.findByCvUrlEquals(uri).getCvName();
+            }
+        }
+        return new FileDownloadVO(folderLocation, filename);
+    }
+
+    public FileDownloadVO downloadImageFile(String type, String uri) { //이미지 파일 다운로드
+        Path folderLocation = null;
+
+        if (type.equals("member")) {
+            folderLocation = Paths.get(game_image_path);
+        }
+        else if (type.equals("detail")) {
+            folderLocation = Paths.get(detail_image_path);
+        }
+        else if (type.equals("main")) {
+            folderLocation = Paths.get(main_image_path);
+        }
+        else {
+            throw new InvalidInputException("유효하지 않은 경로입니다.");
+        }
+
+        String filename = "";
+        if (type.equals("member")) {
             if (cvRepo.findByCvUrlEquals(uri)==null) {
                 throw new InvalidInputException("파일이 존재하지 않습니다.");
             }
