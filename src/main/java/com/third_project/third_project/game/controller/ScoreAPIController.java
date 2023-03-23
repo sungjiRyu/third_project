dceb83d79abadf9ddcb63f2cf62ff84ad196d615
inwonjung <inwon123@naver.com>
2023-03-23 17:11:01 +0900

    hia14 -2

--- src/main/java/com/third_project/third_project/game/controller/ScoreAPIController.java
+++ src/main/java/com/third_project/third_project/game/controller/ScoreAPIController.java
@@ -64,7 +64,7 @@ public class ScoreAPIController {
 //        return new ResponseEntity<>(response, response.getCode());
 //    }
     @Operation(summary = "전체 게임 성적 상위 % 입력", description = "운동 번호를 통해 저번 주 게임 성적 상위 % 테이블에 저장 기능")
-    @PutMapping("/addPercent/{seq}")
+    @PatchMapping("/addPercent/{seq}")
     public ResponseEntity<BasicResponseVO> setAvailableStamp(@Parameter(description = "운동번호", example = "2") @PathVariable Long seq){
         BasicResponseVO response = scoreService.setPercent(seq);
         return new ResponseEntity<>(response, response.getCode());
