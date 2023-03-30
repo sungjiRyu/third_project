65079526f0502ea472f2f09ec64971c5aa6bdf29
armycar <credit102@naver.com>
2023-03-30 09:12:52 +0900

    테스트코드

---
+++ src/test/java/com/third_project/third_project/chaTest/gameTests.java
@@ -0,0 +1,13 @@
+package com.third_project.third_project.chaTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class gameTests {
+
+	@Test
+	void contextLoads() {
+	}
+	
+}
